64a67056cb79d89b1afbe2c041687cea7b039464
Tyler Price <94349608+tprice117@users.noreply.github.com>
2022-05-03 16:26:46 -0500

    git desktop creation

--- pjtsite/pjtsite/asgi.py
+++ pjtsite/pjtsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoproject project.
+ASGI config for pjtsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pjtsite.settings')
 
 application = get_asgi_application()
