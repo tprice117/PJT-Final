187f753741e86a43cc02f6faf0583204bda674d8
Tyler Price <94349608+tprice117@users.noreply.github.com>
2022-10-10 00:20:45 -0500

    QuerySolution

--- pjtsite/pjtapp/admin.py
+++ pjtsite/pjtapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import OrderItems
+from .models import *
 # Register your models here.
+admin.site.register(Orders)
 admin.site.register(OrderItems)
--- pjtsite/pjtapp/views.py
+++ pjtsite/pjtapp/views.py
@@ -1,37 +1,64 @@
 from pipes import Template
+from typing import OrderedDict
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-
+from django import template
 from .models import *
 from .models import OrderItems
 from django.utils.translation import gettext as _
 from django.views.generic.base import TemplateView
 from scripts import orders_load
 from django.views.generic import TemplateView, ListView
-# Create your views here.
-
-# class home(TemplateView):
-#   template_name = 'home.html'
-#   def get_context_data(self, **kwargs):
-#       context = super().get_context_data(**kwargs)
-#       context["ordersSet"] = Orders.objects.all()
-#       return context
-
-# class homeListView(ListView):
-#   template_name = 'home.html'
-#   context_object_name = 'orders_list'
-#   def get_queryset(self):
-#     return Orders.objects.all()
-
 def home(request):
-  # contextmap = {}
-  # contextmap["orderItemsSet"] = OrderItems.objects.select_related()
-  # contextmap["ordersSet"] = Orders.objects.select_related()
-  ojoinoi = OrderItems.objects.all()
-  # orders = Orders.objects.all()
-  return render(request, 'home.html', {'ojoinoi': ojoinoi})
-  
+  orders = list(Orders.objects.values())
+  orderitems = list(OrderItems.objects.values())
+  printmodels = list(PrintModels.objects.values())
+  i=0
+  j=0
+  newOrderList=[]
+  length = len(orders)
+  while(i < len(orders)-3):
+    itemList = []
+    order1 = {"FullName":orders[i]['FullName'], "OrdersID":orders[i]['OrdersID'], 
+    "SaleDate":orders[i]['SaleDate'], "RequiredShipDate":orders[i]['RequiredShipDate']}
+    while(j < len(orderitems) and orderitems[j]['OrdersID_id'] == orders[i]['OrdersID']):
+      dictEntry = {'OrdersID':orders[i]['OrdersID'], 'OrderSKU': orderitems[i]['ItemSKU_id'],
+      'OrderQuantity': orderitems[i]['OrderQuantity']}
+      
+      itemList.append(dictEntry)
+      j+=1
+    order1['itemList'] = itemList
+    newOrderList.append(order1)
+    i+=1
+
+    # if (orderitems[i]["OrdersID_id"] == orders[i]["OrdersID"]):
+    #   itemList.append(orderitems[i])
+
+      
+  #     {'FirstName': 'Shingo',
+  #   'FullName': 'Shingo Sakurai',
+  #   'LastName': 'Sakurai',
+  #   'OrderCompleted': False,
+  #   'OrderPlatform': 'Etsy',
+  #   'OrdersID': 2343347323,
+  #   'RequiredShipDate': datetime.date(2022, 1, 26),
+  #   'SaleDate': datetime.date(2022, 1, 12)},
+
+      # orders[i]["OrdersID"] = "h"
+      # orderid = orders[i]["OrdersID"]
+      # if orderitems[i] == orders[i]["OrdersID"]:
+      #   orderitems = orderitems.filter(OrdersID__exact=orders[i]["OrdersID"])
+      # orders[i]["items"] = orderitems[i]
+
+      # if orderitems[i] == orders[i]["OrdersID"]:
+      #   orders[i]["items"] = OrderItems[i]
+      # if orders[i]["OrdersID"] == orderitems["OrdersID_id"]:
+      #   orders[i]["items"] = orderitems
+      # if (orders[i]["OrdersID"] == 10761):
+      #   orders[i]["OrdersID"] = "f"
+  return render(request, 'home.html', {'orders' : orders, 'orderitems' : orderitems,'order1' : order1, 'itemList' : itemList, 'length' : length, 'newOrderList' : newOrderList})
 # return render(request, 'home.html', {'orderItems': orderItems, 'orders': orders} )
+
 def uploadorders(request):
   return render(request, 'uploadorders.html')
   
